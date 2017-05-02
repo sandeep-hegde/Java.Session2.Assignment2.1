cba1e9cd3c1d16282743a1ecb6f1b53d1c3de012
Sandeep Hegde <sandeep.hegde123@gmail.com>
2017-05-02 20:58:17 +0530

    Java.Session2.Assignment2.1
    Write a program to print your name without using semicolon in the
    program.

---
+++ Assignment2.1/src/JavaSession2Assignment2_1.java
@@ -0,0 +1,14 @@
+
+//Problem Statement - Write a program to print your name without using semicolon in the program.
+
+public class JavaSession2Assignment2_1 {
+
+	public static void main(String[] args) {
+
+		if (System.out.printf("My name is Sandeep") == null) {
+
+		}
+
+	}
+
+}
